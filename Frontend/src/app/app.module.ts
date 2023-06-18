2e77d83f0bde595c304cc10fbf45fbdee4d300b6
SKsk4585 <5864585sari@gmail.com>
2023-06-18 21:33:18 +0300

    end test

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { LayoutComponent } from './components/layout-area/layout/layout.component';
 import { MenuComponent } from './components/layout-area/menu/menu.component';
 import { HttpClientModule } from "@angular/common/http";
-import { HomeComponent } from './components/home-area/home/home.component';
+
 import { ListComponent } from './components/data-area/list/list.component';
 import { AddComponent } from './components/data-area/add/add.component';
 import { PageNotFoundComponent } from './components/layout-area/page-not-found/page-not-found.component';
@@ -14,7 +14,6 @@ import { PageNotFoundComponent } from './components/layout-area/page-not-found/p
     declarations: [
         LayoutComponent,
         MenuComponent,
-        HomeComponent,
         ListComponent,
         AddComponent,
         PageNotFoundComponent
